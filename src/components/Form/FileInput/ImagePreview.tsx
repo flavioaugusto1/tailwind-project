b79b6d4cc0462cd887ac0b904b7cbf9e0fd6541a
Flávio <f.augustosdn@gmail.com>
2024-02-15 17:38:15 -0300

    fix: agora está sendo exibido o placeholder quando não tiver imagem selecionada

--- src/components/Form/FileInput/ImagePreview.tsx
+++ src/components/Form/FileInput/ImagePreview.tsx
@@ -11,11 +11,10 @@ export function ImagePreview() {
     if (files.length === 0) {
       return
     }
-
     return URL.createObjectURL(files[0])
   }, [files])
 
-  if (previewURL === null) {
+  if (!previewURL) {
     return (
       <div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50">
         <User className="h-8 w-8 text-violet-500" />
